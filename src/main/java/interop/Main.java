0668ecb889714b11c792967f19d738ef716178e1
Mark Bastian <markbastian@gmail.com>
2017-05-17 20:01:13 -0600

    new examples

--- src/main/java/interop/Main.java
+++ src/main/java/interop/Main.java
@@ -1,8 +1,12 @@
 package interop;
 
+import interop.protocols_and_records.Person;
+import interop.api.*;
+
 import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args){
@@ -26,5 +30,16 @@ public class Main {
         Collection<String> methodJ = demo.methodJ(doubles);
         System.out.println(methodC);
         System.out.println(methodJ);
+
+        JInterface o = FactoryDemo.getInstance();
+        final Collection<String> s = o.methodJ(doubles);
+        System.out.println("Factory method: " + s);
+
+        System.out.println(s.stream().map(String::length).collect(Collectors.toList()));
+
+        Person mark = new Person("Mark", 21);
+        System.out.println(mark);
+        System.out.println(((Person)mark.getOlder()).age);
+        System.out.println(mark.pretty());
     }
 }
